b57ad3483ecf0bf219af19c3002958c427beca5d
Finley Lau <finleylau98@gmail.com>
2018-07-10 13:37:40 -0500

    Starter code

---
+++ assignment2.py
@@ -0,0 +1,10 @@
+def printHello():
+  # FILL ME IN
+  return
+
+def printBye():
+  # FILL ME IN
+  return
+
+printHello()
+printBye()
---
+++ readme.txt
@@ -0,0 +1,7 @@
+Assignment2.py must print Hello and Bye to the console.
+
+Hello:
+print(Hello)
+
+Bye:
+print(Bye)
